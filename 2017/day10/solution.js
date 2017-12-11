3d20359ccbe207d28f38f1070e9bc80dfc5a0016
Petr Kolář <kolarcz@gmail.com>
2017-12-12 00:22:23 +0100

    Day 10

---
+++ 2017/day10/solution.js
@@ -0,0 +1,62 @@
+function createNumbers(length = 256) {
+  return [...new Array(length)].map((tmp, i) => i);
+}
+
+function reverseNumbers(numbers, pos, length) {
+  const halfLength = Math.ceil(length / 2);
+
+  for (let i = 0; i < halfLength; i++) {
+    const a = (pos + i) % numbers.length;
+    const b = (pos + (length - 1 - i)) % numbers.length;
+    [numbers[a], numbers[b]] = [numbers[b], numbers[a]];
+  }
+}
+
+function runLengths(numbers, lengths, curr = 0, skip = 0) {
+  let currentPosition = curr;
+  let skipSize = skip;
+
+  for (let i = 0; i < lengths.length; i++) {
+    const length = lengths[i];
+    reverseNumbers(numbers, currentPosition, length);
+    currentPosition += length + skipSize++;
+  }
+
+  return { currentPosition, skipSize };
+}
+
+function getHash(numbers) {
+  const numbs = [...numbers];
+  let hash = '';
+
+  while (numbs.length) {
+    const xor = numbs.splice(0, 16).reduce((a, b) => (a ^ b));
+    const ch = xor.toString(16);
+    hash += (ch.length > 1 ? '' : '0') + ch;
+  }
+
+  return hash;
+}
+
+exports.part1 = (input, numbsLength = 256) => {
+  const lengths = input.split(',').map(Number);
+  const numbers = createNumbers(numbsLength);
+
+  runLengths(numbers, lengths);
+
+  const result = numbers[0] * numbers[1];
+  return result;
+};
+
+exports.part2 = input => {
+  const lengths = [...input].map(ch => ch.charCodeAt(0)).concat([17, 31, 73, 47, 23]);
+  const numbers = createNumbers();
+
+  let [currentPosition, skipSize] = [0, 0];
+  for (let i = 0; i < 64; i++) {
+    ({ currentPosition, skipSize } = runLengths(numbers, lengths, currentPosition, skipSize));
+  }
+
+  const hash = getHash(numbers);
+  return hash;
+};
---
+++ 2017/day10/test.js
@@ -0,0 +1,17 @@
+const test = require('tape');
+const { part1, part2 } = require('./solution');
+
+test('day10', t => {
+  t.test('part1', t => {
+    t.equal(part1('3, 4, 1, 5', 5), 12);
+    t.end();
+  });
+
+  t.test('part2', t => {
+    t.equal(part2(''), 'a2582a3a0e66e6e86e3812dcb672a272');
+    t.equal(part2('AoC 2017'), '33efeb34ea91902bb2f59c9920caa6cd');
+    t.equal(part2('1,2,3'), '3efbe78a8d82f29979031a4aa0b16a9d');
+    t.equal(part2('1,2,4'), '63960835bcdc130f0b66d7ff4f6a5a8e');
+    t.end();
+  });
+});
