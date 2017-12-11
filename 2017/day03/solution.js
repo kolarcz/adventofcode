42f8a5682c844d61352a8054de1b06bc0de4e21e
Petr Kolář <kolarcz@gmail.com>
2017-12-11 17:14:56 +0100

    Day 3

---
+++ 2017/day03/solution.js
@@ -0,0 +1,62 @@
+exports.part1 = input => {
+  const number = Number(input);
+
+  if (number <= 1) return 0;
+
+  let max = 1;
+  let depth = 0;
+
+  while (max < number) {
+    max += ++depth * 8;
+  }
+
+  const side = depth * 2;
+  const sideSteps = Math.abs(((max - number) % side) - (side / 2));
+  const steps = sideSteps + depth;
+
+  return steps;
+};
+
+exports.part2 = input => {
+  const number = Number(input);
+
+  const numbers = {};
+  let [x, y] = [0, 0];
+  let [xd, yd] = [0, 0];
+  let bigger = 0;
+  let depth = 0;
+
+  const neighboursSum = (x, y) =>
+    [[1, -1], [1, 0], [1, 1], [0, -1], [0, 1], [-1, -1], [-1, 0], [-1, 1]]
+      .reduce((sum, [a, b]) =>
+        sum + (numbers[[x + a, y + b]] || 0),
+      0);
+
+  const moveToNext = () => {
+    if (x === depth && y === depth) {
+      xd = 0; yd = -1;
+    } else if (x === depth && y === -depth) {
+      xd = -1; yd = 0;
+    } else if (x === -depth && y === -depth) {
+      xd = 0; yd = 1;
+    } else if (x === -depth && y === depth) {
+      xd = 1; yd = 0;
+    }
+
+    if (x === depth && y === depth) {
+      depth++;
+      x++;
+    } else {
+      x += xd;
+      y += yd;
+    }
+  };
+
+  while (bigger <= number) {
+    bigger = neighboursSum(x, y) || 1;
+    numbers[[x, y]] = bigger;
+    moveToNext();
+  }
+
+  return bigger;
+};
---
+++ 2017/day03/test.js
@@ -0,0 +1,21 @@
+const test = require('tape');
+const { part1, part2 } = require('./solution');
+
+test('day03', t => {
+  t.test('part1', t => {
+    t.equal(part1('1'), 0);
+    t.equal(part1('12'), 3);
+    t.equal(part1('23'), 2);
+    t.equal(part1('1024'), 31);
+    t.end();
+  });
+
+  t.test('part2', t => {
+    t.equal(part2('1'), 2);
+    t.equal(part2('2'), 4);
+    t.equal(part2('3'), 4);
+    t.equal(part2('4'), 5);
+    t.equal(part2('5'), 10);
+    t.end();
+  });
+});
