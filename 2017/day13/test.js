3e24ee00481d95586341bea5125a733e6b636646
Petr Kolář <kolarcz@gmail.com>
2017-12-17 14:52:43 +0100

    Days 13, 14, 16 tests, inputs & readme

---
+++ 2017/day13/test.js
@@ -0,0 +1,24 @@
+const test = require('tape');
+const { part1, part2 } = require('./solution');
+
+test('day13', t => {
+  t.test('part1', t => {
+    t.equal(part1(
+      '0: 3\n' +
+      '1: 2\n' +
+      '4: 4\n' +
+      '6: 4'
+    ), 24);
+    t.end();
+  });
+
+  t.test('part2', t => {
+    t.equal(part2(
+      '0: 3\n' +
+      '1: 2\n' +
+      '4: 4\n' +
+      '6: 4'
+    ), 10);
+    t.end();
+  });
+});
---
+++ 2017/day14/test.js
@@ -0,0 +1,14 @@
+const test = require('tape');
+const { part1, part2 } = require('./solution');
+
+test('day14', t => {
+  t.test('part1', t => {
+    t.equal(part1('flqrgnkx'), 8108);
+    t.end();
+  });
+
+  t.test('part2', t => {
+    t.equal(part2('flqrgnkx'), 1242);
+    t.end();
+  });
+});
