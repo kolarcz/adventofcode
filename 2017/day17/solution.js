816b149f2e78960d20b332b9441ba50275c67271
Petr Kolář <kolarcz@gmail.com>
2017-12-17 14:41:59 +0100

    Day 17

---
+++ 2017/day17/solution.js
@@ -0,0 +1,26 @@
+exports.part1 = input => {
+  const spin = Number(input);
+  const arr = [0];
+  let pos = 0;
+
+  for (let i = 1; i <= 2017; i++) {
+    pos = ((pos + spin) % arr.length) + 1;
+    arr.splice(pos, 0, i);
+  }
+
+  const pos2017 = arr.indexOf(2017);
+  return arr[pos2017 + 1];
+};
+
+exports.part2 = input => {
+  const spin = Number(input);
+  let number = 0;
+  let pos = 0;
+
+  for (let i = 1; i <= 50000000; i++) {
+    pos = ((pos + spin) % i) + 1;
+    if (pos === 1) number = i;
+  }
+
+  return number;
+};
---
+++ 2017/day17/test.js
@@ -0,0 +1,13 @@
+const test = require('tape');
+const { part1 } = require('./solution');
+
+test('day17', t => {
+  t.test('part1', t => {
+    t.equal(part1('3'), 638);
+    t.end();
+  });
+
+  t.test('part2', t => {
+    t.end();
+  });
+});
